4b081c612e3bf9d799e03b4873f9758c8c0d5af6
Ovodo <ovdizzle@gmail.com>
2023-06-17 10:33:33 +0100

    image resolution

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
+  // output: "export",
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -10,6 +10,18 @@ const nextConfig = {
         hostname: "s3-alpha-sig.figma.com",
         port: "",
         pathname: "/img/2e73/**",
+      },
+      {
+        protocol: "https",
+        hostname: "scontent.fabv2-2.fna.fbcdn.net",
+        port: "",
+        pathname: "/v/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/**",
       },
     ],
   },
