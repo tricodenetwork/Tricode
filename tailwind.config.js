3fa7bfc812b1df88deb63b9d72d5056ebad1287b
Ovodo <ovdizzle@gmail.com>
2023-09-07 14:44:21 +0100

    changed component casing in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,13 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       screen: {
         s: "375px",
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
         primary: "#25092c",
