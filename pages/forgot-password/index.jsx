6d2d620faab5527fdba306f308135b6b9edd1127
upraisertech <akoredesalaudeen54@gmail.com>
2023-09-10 21:58:27 +0100

    Set New password

--- pages/forgot-password/index.jsx
+++ pages/forgot-password/index.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import InputLine from "@/components/InputLine";
 import { useState } from "react";
 import Sidebar from "@/Components/layouts/Sidebar";
+import Image from "next/image";
 
 const Index = () => {
   // --------------------------------------------VARIABLES
@@ -30,7 +31,16 @@ const Index = () => {
               </div>
               <Link href='/sms-verification'>
                 <div className="w-[106px] h-[82px] md:h-[90px] bg-zinc-300 rounded-tr-[10px] justify-center items-center rounded-br-[10px] border border-zinc-300">
-                  <div className="w-9 h-9 m-10">img</div>
+                  <div className="w-9 h-9 m-7">
+                    <Image
+                      alt='logo'
+                      width={80}
+                      height={40}
+                      quality={100}
+                      className='w-[10vw]'
+                      src='/assets/icons/arrow-right-alt.svg'
+                    />
+                  </div>
                 </div>
               </Link>
             </div>
@@ -46,7 +56,16 @@ const Index = () => {
               </div>
               <Link href='/sms-verification'>
                 <div className="w-[106px] h-[82px] md:h-[90px] bg-zinc-300 rounded-tr-[10px] justify-center items-center rounded-br-[10px] border border-zinc-300">
-                  <div className="w-9 h-9 m-10">img</div>
+                  <div className="w-9 h-9 m-7">
+                    <Image
+                      alt='logo'
+                      width={80}
+                      height={40}
+                      quality={100}
+                      className='w-[10vw]'
+                      src='/assets/icons/arrow-right-alt.svg'
+                    />
+                  </div>
                 </div>
               </Link>
             </div>
--- pages/sms-verification/index.jsx
+++ pages/sms-verification/index.jsx
@@ -4,6 +4,7 @@ import InputLine from "@/components/InputLine";
 import { useState } from "react";
 import Sidebar from "@/Components/layouts/Sidebar";
 import Button from "@/components/Button";
+import OTPInput from "@/Components/OTPInput";
 
 const Index = () => {
   // --------------------------------------------VARIABLES
@@ -19,12 +20,8 @@ const Index = () => {
         <div className='bg-midorang mx-auto login min-h-max flex flex-col px-3 justify-center items-center'>
           <h3>SMS Verification</h3>
           <div className="mt-5">
-            <div className="w-[424px] text-zinc-500 text-lg font-normal">A text message with a six digit verification code has been sent to your phone number ending in X  X  X  X  X  X 6 0 9 7</div>
-            <input
-              type='text'
-              placeholder="code"
-              className='border-b-2 mt-3 bg-neutral-100 border-gray-400 focus:outline-none focus:border-b-2 focus:border-binance_green w-full'
-            />
+            <div className="w-[424px] text-zinc-500 mb-2 text-lg font-normal">A text message with a six digit verification code has been sent to your phone number ending in X  X  X  X  X  X 6 0 9 7</div>
+            <OTPInput length={6} onChange={(value, index) => console.log(`Digit ${index + 1}: ${value}`)} />
             <div class="text-binance_green mt-3 text-sm md:text-xl font-semibold">Send another code</div>
             <div className='w-full mt-4'>
               <Button styles={"w-[60%] md:w-full mx-auto"} Action={"Continue"} />
