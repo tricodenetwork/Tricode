2859b8fd5cbd2d2055a54c138ede1cece5174353
Ovodo <ovdizzle@gmail.com>
2023-10-10 00:45:15 +0100

    notifications modal done

---
+++ pages/test.jsx
@@ -0,0 +1,15 @@
+import ChangePassword from "@/components/modals/ChangePassword";
+import ModalComponent from "@/components/modals/ModalComponent";
+import Notifications from "@/components/modals/Notifications";
+
+const test = () => {
+  // --------------------------------------------VARIABLES
+
+  //-----------------------------------------------------------FUNCTIONS
+
+  //------------------------------------------------------------------USE EFFECTS
+
+  return <ModalComponent Content={Notifications} />;
+};
+
+export default test;
