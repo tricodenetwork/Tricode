cf54c9e0a32dce3801dd97ae6153194c92cd0d43
upraisertech <akoredesalaudeen54@gmail.com>
2023-12-17 10:49:15 +0100

    Hire Tricode

--- pages/index.js
+++ pages/index.js
@@ -81,73 +81,11 @@ export default function Home() {
             loop={true}
           /> */}
         </section>
-        
-        <motion.section
-          className="relative top-[0vh]  overflow-hidden h-[100vh]"
-          ref={ref}
-          id="aboutUs"
-        >
-          <GraySquare />
-          <motion.p
-            animate={animation}
-            style={{ lineHeight: 2 }}
-            className="w-[50vw] absolute top-[14vh] right-[2vw] text-white text-[15px] md:text-[20px]"
-          >
-            TRICODE Network! We take great pride in being a distinguished
-            platform that connects exceptional talents in the tech industry with
-            organizations seeking to bring their visionary ideas to life. We
-            unite top-notch developers for transformative projects that shape
-            the future, fostering collaboration and positive impact for all.
-            Join us to be part of something great.
-            <button className="font-medium scale-50 hover:bg-opacity-70 hover:scale-[.55] transition ease-linear duration-150 shadow-sm shadow-secondary px-[7vw] py-[1.5vh] rounded-lg bg-black text-white">
-              JOIN
-            </button>
-          </motion.p>
-          <motion.p
-            animate={animation2}
-            className="w-[30vw] absolute top-[68vh] left-[2vw] bg-black p-2 rounded-sm text-white text-[17px]"
-          >
-            {`We foster a secure and inclusive <Developer /> community, connecting
-          diverse skilled developers to collaborate on challenging projects,
-          creating cutting-edge solutions in utility software, <Mechatronics />
-          engineering, and humanitarian initiatives. Join our network platform
-          to share ideas, knowledge, and resources, driving innovation and
-          impacting the world positively.`}
-          </motion.p>
-          <motion.p
-            animate={animation}
-            className="w-[30vw] absolute top-[68vh] left-[58vw]  bg-black p-2 rounded-sm text-white text-[17px]"
-          >
-            {`
-Our goal is to create sustainable technology innovations that help solve some of the most pressing challenges facing our world today, particularly those related to climate change and green energy. We believe that Tricode Network is well positioned to make a significant impact on the world of technology and beyond.
-`}
-          </motion.p>
-        </motion.section>
-        <section
-          id="patners"
-          className="mt-[40vh] pb-[5vh] relative h-[100vh] justify-center"
-        >
-          <p className="absolute text-[28px] text-white self-center w-full text-center mt-[2vh] font-semibold ">{`< Patners />`}</p>
-          <Square />
-          <div className="px-[5vw]">
-            <Image
-              width={200}
-              height={80}
-              quality={100}
-              className="border-2 top-[20vh] bg-white right-[10vw] absolute w-[35vw]"
-              src="/assets/images/switch.jpg"
-              alt="switch"
-            />
 
-            <Image
-              width={200}
-              height={100}
-              quality={100}
-              alt="deltasciencenigeria"
-              className=" border-2 top-[20vh] bg-white absolute w-[35vw]"
-              src="/assets/images/dsn.png"
-            />
-          </div>
+        <section
+          id="footer"
+          className="mt-[20vh] relative justify-center"
+        >
           <div className="px-[2vw] flex justify-between mb-[15vh]">
             <p className="text-black text-sm">&copy; 2023 tricode.pro</p>
             <div className="w-[30vw]  flex justify-around">
