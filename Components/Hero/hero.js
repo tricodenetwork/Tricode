67eb3e4ae45a700d3c6a45739b6d3ff9752986e4
vveisman <vicent914a@gmail.com>
2023-12-21 18:58:20 +0100

    hero section fix

--- Components/Hero/hero.js
+++ Components/Hero/hero.js
@@ -51,15 +51,15 @@ export default function Home() {
 
   return (
     <>
-      <section className="h-[700px] overflow-hidden" id="hero">
+      <section className='h-screen relative overflow-hidden' id='hero'>
         <div>
           <img
-            className="h-screen sm:h-[80vh] w-full"
-            src="/assets/images/black-background.png"
+            className='h-screen w-full'
+            src='/assets/images/black-background.png'
           />
         </div>
-        <div className="absolute mx-[3vw] top-[30vh] md:bottom-[30vh]">
-          <div className="header relative max-w-max">
+        <div className='absolute mx-[3vw] top-[30vh] md:bottom-[30vh]'>
+          <div className='header relative max-w-max'>
             <span className="text-white text-[53px] font-extrabold font-['Inter'] leading-[71px]">
               Build{" "}
             </span>
@@ -79,24 +79,24 @@ export default function Home() {
           <div
             className={`flex justify-between space-x-4 mt-[0vh] md:space-x-6 md:mt-[3vh]`}
           >
-            <button className="font-medium hover:bg-opacity-70 hover:scale-110 transition ease-linear duration-150 shadow-sm shadow-black px-[7vw] py-[1.5vh] rounded-lg bg-binance_green text-white">
+            <button className='font-medium hover:bg-opacity-70 hover:scale-110 transition ease-linear duration-150 shadow-sm shadow-black px-[7vw] py-[1.5vh] rounded-lg bg-binance_green text-white'>
               HIRE
             </button>
-            <button className="font-medium hover:bg-opacity-70 hover:scale-110 transition ease-linear duration-150 shadow-sm shadow-binance_green px-[7vw] py-[1.5vh] rounded-lg bg-black text-white">
+            <button className='font-medium hover:bg-opacity-70 hover:scale-110 transition ease-linear duration-150 shadow-sm shadow-binance_green px-[7vw] py-[1.5vh] rounded-lg bg-black text-white'>
               JOIN
             </button>
           </div>
         </div>
-        <div className="absolute top-[23vh] right-[5vw]">
+        <div className='absolute top-[23vh] right-[5vw]'>
           <Image
             loader={imageLoader}
             width={!mobile ? 400 : 200}
             height={700}
-            src="/assets/lottie/amico.png"
-            alt="hand"
+            src='/assets/lottie/amico.png'
+            alt='hand'
           />
         </div>
-        <div className="absolute top-[63vh] mx-3 w-full overflow-hidden">
+        <div className='absolute bottom-5 mx-3 w-full overflow-hidden'>
           <Marq />
         </div>
       </section>
