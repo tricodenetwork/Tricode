0365eee6440257cc8c581686f6a36cf79f48f64d
vveisman <vicent914a@gmail.com>
2023-12-23 20:57:47 +0100

    Revert "update Navbar"
    This reverts commit 2910ff76da03f20ea5dd10b237968c3665a2e2e0.

--- Components/navbar_components/Navbar.js
+++ Components/navbar_components/Navbar.js
@@ -1,16 +1,14 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { MdOutlineMenu } from "react-icons/md";
-import { IoCloseSharp } from "react-icons/io5";
+import React, { useCallback, useEffect } from "react";
+
+import { useState } from "react";
 import Link from "next/link";
 import { motion, useAnimation } from "framer-motion";
 import Image from "next/image";
 import LinkItem from "../LinkItem";
 import useFunctions from "@/hooks/useFunctions";
-import SideNavMobile from "./sidenavmobile";
 
 const Navbar = ({ children }) => {
   const [showmenu, setShowmenu] = useState(false);
-  const [sideNav, setSideNav] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
   const { imageLoader } = useFunctions();
 
@@ -38,8 +36,8 @@ const Navbar = ({ children }) => {
   return (
     <div
       className={`${
-        isScrolled || sideNav ? "bg-binance_green" : "bg-transparent"
-      } fixed h-[60px] md:h-[90px] w-full z-50 flex flex-col`}
+        isScrolled ? "bg-binance_green" : "bg-transparent"
+      } fixed h-[90px] w-full z-50 flex flex-col`}
     >
       <motion.nav
         initial={{ x: 0 }}
@@ -56,7 +54,7 @@ const Navbar = ({ children }) => {
           //   repeat: 7,
           //   duration: 4,
           // }}
-          className="logos text-white"
+          className="logos hidden  text-white"
         >
           <Image
             loader={imageLoader}
@@ -68,76 +66,67 @@ const Navbar = ({ children }) => {
             src="/assets/images/logo.png"
           />
         </motion.div>
-        <div className="w-full gap-7 lg:gap-12 hidden md:flex justify-center items-start">
-          <LinkItem
-            hideMenu={hideMenu}
-            path="/about"
-            offColor={false}
-            text="About us"
-            isScrolled={isScrolled}
-          />
-          <LinkItem
-            hideMenu={hideMenu}
-            path="/services"
-            offColor={false}
-            text="Services"
-            isScrolled={isScrolled}
-          />
-          <LinkItem
-            hideMenu={hideMenu}
-            path="/projects"
-            offColor={false}
-            text="Projects"
-            isScrolled={isScrolled}
-          />
-          <LinkItem
-            hideMenu={hideMenu}
-            path="/communities"
-            text="Communities"
-            isScrolled={isScrolled}
-          />
-          <LinkItem
-            hideMenu={hideMenu}
-            path="#newsletter"
-            text="Newsletter"
-            isScrolled={isScrolled}
-          />
-        </div>
-        <motion.div
-          initial={{ width: "40%" }}
-          animate={{ width: "20%" }}
-          // transition={{ type: "spring", stiffness: 500, duration: 0.1 }}
-          transition={{ duration: 0.2 }}
-          className="icon flex w-[40%] md:w-[20%] justify-around  items-center"
-        >
-          <LinkItem
-            text={"Register"}
-            path={"/menu"}
-            className={
-              "medText p-3 rounded-lg md:flex hidden border border-slate-500"
-            }
-            isScrolled={isScrolled}
-          />
-          <LinkItem
-            hideMenu={hideMenu}
-            path="/login"
-            text="Login"
-            className={`medText text-[white] md:flex hidden`}
-            isScrolled={isScrolled}
-          />
-        </motion.div>
-
-        <div
-          onClick={() => setSideNav(!sideNav)}
-          className="text-white flex md:hidden"
-        >
-          {sideNav ? (
-            <IoCloseSharp className="text-[3vh]" />
-          ) : (
-            <MdOutlineMenu className="text-[3vh]" />
-          )}
-        </div>
-        {sideNav && <SideNavMobile />}
+          <div
+            className="nav-menu w-[55%] hidden md:flex"
+            id={showmenu ? "mobile" : "hide"}
+          >
+            <LinkItem
+              hideMenu={hideMenu}
+              path="/about"
+              offColor={false}
+              text="About us"
+              isScrolled={isScrolled}
+            />
+            <LinkItem
+              hideMenu={hideMenu}
+              path="/services"
+              offColor={false}
+              text="Services"
+              isScrolled={isScrolled}
+            />
+            <LinkItem
+              hideMenu={hideMenu}
+              path="/projects"
+              offColor={false}
+              text="Projects"
+              isScrolled={isScrolled}
+            />
+            <LinkItem
+              hideMenu={hideMenu}
+              path="/communities"
+              text="Communities"
+              isScrolled={isScrolled}
+            />
+            <LinkItem
+              hideMenu={hideMenu}
+              path="#newsletter"
+              text="Newsletter"
+              isScrolled={isScrolled}
+            />
+          </div>
+          <motion.div
+            initial={{ width: "40%" }}
+            animate={{ width: "20%" }}
+            // transition={{ type: "spring", stiffness: 500, duration: 0.1 }}
+            transition={{ duration: 0.2 }}
+            className="icon flex w-[40%] md:w-[20%] justify-around  items-center"
+          >
+            <LinkItem
+              text={"Register"}
+              path={"/menu"}
+              className={
+                "medText p-3 rounded-lg md:flex hidden border border-slate-500"
+              }
+              isScrolled={isScrolled}
+            />
+            <LinkItem
+              hideMenu={hideMenu}
+              path="/login"
+              text="Login"
+              className={`medText text-[white] md:flex hidden`}
+              isScrolled={isScrolled}
+            />
+          </motion.div>
       </motion.nav>
     </div>
   );
