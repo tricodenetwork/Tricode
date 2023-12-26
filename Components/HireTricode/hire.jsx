4409db38a2c7a47258752856ab83f3ce0dee0f5e
upraisertech <akoredesalaudeen54@gmail.com>
2023-12-26 12:29:16 +0100

    updates

--- Components/HireTricode/hire.jsx
+++ Components/HireTricode/hire.jsx
@@ -6,64 +6,16 @@ import { IconWifi } from "@tabler/icons-react";
 import { IconUsersGroup } from "@tabler/icons-react";
 import { IconWand } from "@tabler/icons-react";
 import HireCard from "./card";
+import Connect from "./connect";
 
 export default function HireTricode({ mobile }) {
   return (
     <>
+    <Connect />
       <section
         className="px-8 py-5 w-full bg-gradient-to-l from-[#BBD2B3] to-stone-100 overflow-hidden justify-center items-center"
         id="intro"
       >
-        <div
-          className={`flex flex-col md:flex-row gap-7 justify-center md:justify-between items-center border-b w-full py-7`}
-        >
-          <button className="flex flex-col-reverse md:flex-row gap-3 justify-center items-center">
-            <div className="text-center justify-center items-center">
-              <div className="text-gray-900 text-2xl font-semibold font-['Poppins']">
-                Connect
-              </div>
-              <div className="w-[206px] text-gray-900 text-sm font-normal font-['Plus Jakarta Sans'] leading-normal">
-                Connect with us to get your ideas to reality
-              </div>
-            </div>
-            <img
-              src="/assets/icons/airdrop.svg"
-              className="bg-[#888484] p-3 rounded-[30px]"
-              alt=""
-            />
-          </button>
-          <button className="flex flex-col-reverse md:flex-row gap-3 justify-center items-center">
-            <div className="text-center justify-center items-center">
-              <div className="text-gray-900 text-2xl font-semibold font-['Poppins']">
-                Collaborate
-              </div>
-              <div className="w-[206px] text-gray-900 text-sm font-normal font-['Plus Jakarta Sans'] leading-normal">
-                Collaborate with a team to get solutions to any question
-              </div>
-            </div>
-            <img
-              src="/assets/icons/airdrop.svg"
-              className="bg-[#888484] p-3 rounded-[30px]"
-              alt=""
-            />
-          </button>
-          <button className="flex flex-col-reverse md:flex-row gap-3 justify-center items-center">
-            <div className="text-center justify-center items-center">
-              <div className="text-gray-900 text-2xl font-semibold font-['Poppins']">
-                Create
-              </div>
-              <div className="w-[206px] text-gray-900 text-sm font-normal font-['Plus Jakarta Sans'] leading-normal">
-                Create immediately and iterate quickly
-              </div>
-            </div>
-            <img
-              src="/assets/icons/airdrop.svg"
-              className="bg-[#888484] p-3 rounded-[30px]"
-              alt=""
-            />
-          </button>
-        </div>
-
         <div className="flex flex-col md:flex-row justify-start items-center w-full mt-12 gap-4 md:gap-[19vh]">
           <div className="flex-col justify-start items-start gap-5 inline-flex">
             <div className="text-binance_green text-2xl md:text-6xl font-semibold font-['Poppins'] tracking-wide">
