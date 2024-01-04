fbfebe55f679223e15877c304277a7aaba4ee14f
upraisertech <akoredesalaudeen54@gmail.com>
2024-01-04 10:30:50 +0100

    updates

--- Components/HireTricode/join.jsx
+++ Components/HireTricode/join.jsx
@@ -15,7 +15,7 @@ export default function JoinTricode({ mobile }) {
             </div>
             <div className="text-zinc-500 text-xs md:text-base leading-6 tracking-tight">
               Get a partner who understands the dynamic world of technology and
-              innovation.  Tricode is your gateway to harnessing the full
+              innovation. <br/> Tricode is your gateway to harnessing the full
               potential of cutting-edge tech solutions.
             </div>
             <button className="font-bold mt-9 hover:bg-opacity-70 hover:scale-110 transition ease-linear duration-150 shadow-sm shadow-binance_green px-[7vw] py-[1.5vh] rounded-lg bg-black text-white">
