f0ca325ee2ad3ec279bd3659ad1eb6777e21e5a0
vveisman <ovdizzle@gmail.com>
2024-03-02 12:34:46 +0100

    capital letter F in faq folder

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Navbar from "@/Components/navbar_components/Navbar";
 import About from "@/Components/About/index";
-import FAQ from "@/Components/About/faq/faqs";
+import FAQ from "@/Components/About/Faq/faqs";
 import OurNewsletter from "@/Components/OurNewsletter/index";
 import Footer from "@/Components/Footer/index";
 
