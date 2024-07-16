2aaf81832223794a72959e0a41c961e1fd5d4c30
ovodo <ovdizzle@gmail.com>
2024-07-16 03:00:25 +0100

    added green background to hello page

--- pages/client/hello.jsx
+++ pages/client/hello.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Index = () => {
   return (
-    <div className='w-screen h-screen flex items-center justify-center text-white text-4xl'>
+    <div className='w-screen h-screen bg-binance_green flex items-center justify-center text-white text-4xl'>
       HELLO BRO!!!
     </div>
   );
