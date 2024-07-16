38ab8e56f7d6b7989a7f2d4a2c5148c06d527326
ovodo <ovdizzle@gmail.com>
2024-07-16 02:42:52 +0100

    testing subdomain routing

---
+++ pages/client/hello.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center text-white text-4xl'>
+      HELLO BRO!!!
+    </div>
+  );
+};
+
+export default Index;
