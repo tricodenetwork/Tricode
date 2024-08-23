9654cd5b0a7ac916c7e5f15d9dff24e45f9705b4
ovodo <ovdizzle@gmail.com>
2024-08-23 14:32:04 +0100

    added grapgh ql logic to main

---
+++ pages/api/graphql/index.js
@@ -0,0 +1,30 @@
+import Users from "@/lib/db/sources";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { ApolloServer } from "@apollo/server";
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
+import Projects from "@/lib/db/sources/project";
+import clientPromise from "@/lib/mongodb";
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+});
+
+export default startServerAndCreateNextHandler(server, {
+  context: async (req, res) => {
+    const client = await clientPromise;
+    const db = client.db("Tricode");
+    const ProjectModel = await db.collection("Projects");
+    const UserModel = await db.collection("users");
+
+    return {
+      req,
+      res,
+      dataSources: {
+        users: new Users({ modelOrCollection: UserModel }),
+        projects: new Projects({ modelOrCollection: ProjectModel }),
+      },
+    };
+  },
+});
